086aa93a5829c8d631bec5d95630c964ff0ed789
BeranPanasper <contact@alastor-films.com>
2016-03-25 19:31:22 +0100

    (Over) optimize initial loop function

--- dist/bloom-framework.js
+++ dist/bloom-framework.js
@@ -719,19 +719,24 @@
 
 }());
 
+/*global bloom, requestAnimationFrame*/
+(function () {
+    'use strict';
 
-(function() {
     var loop = bloom.ns('core.loop'),
         debug = bloom.ns('core.debug'),
-        core = bloom.ns('core');
+        core = bloom.ns('core'),
 
-    loop.animate = function(time) {
+        instances = core.instances,
+        i,
+        l;
+
+    loop.animate = function (time) {
         requestAnimationFrame(loop.animate);
-        var is = core.instances, i, l = is.length;
-        for (i = 0; i < l; i += 1) {
-            is[i].update(time);
+        for (i = 0, l = instances.length; i < l; i += 1) {
+            instances[i].update(time);
         }
-    }
+    };
 
     loop.animate(0);
 }());
